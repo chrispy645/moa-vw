08c93bfa2984d2500e7881f1282e9fb59c67c255
Christopher Beckham <chrispy645@gmail.com>
2015-09-04 17:36:44 +1200

    Add vw_test stuff

---
+++ vw_test/src/test/java/vw/TestDiabetesNumeric.java
@@ -0,0 +1,24 @@
+package vw;
+
+import weka.core.Instances;
+import weka.core.converters.ConverterUtils.DataSource;
+import moa.ArffToVW;
+
+public class TestDiabetesNumeric {
+	
+	public static void main(String[] args) throws Exception {
+		
+		DataSource ds = new DataSource("datasets/diabetes_numeric.arff");
+		Instances data = ds.getDataSet();
+		data.setClassIndex( data.numAttributes() - 1 );
+		
+		VW learner = new VW("");
+		
+		for(int i = 0; i < data.numInstances(); i++) {
+			String inst = ArffToVW.process( data.get(i) );
+			System.out.println( learner.learn(inst) );
+		}
+		
+	}
+
+}
