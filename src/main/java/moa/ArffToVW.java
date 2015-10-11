98ae115e90b6cb498c3d371ed4ec225171cd9919
Christopher Beckham <chrispy645@gmail.com>
2015-10-11 15:26:33 +1300

    Can convert sparse instances, and fixed bug with nominal attr conversion

--- src/main/java/moa/ArffToVW.java
+++ src/main/java/moa/ArffToVW.java
@@ -5,18 +5,26 @@ import java.io.File;
 import java.io.FileWriter;
 
 import weka.core.Attribute;
+import weka.core.DenseInstance;
 import weka.core.Instance;
 import weka.core.Instances;
 import weka.core.converters.ConverterUtils.DataSource;
 
 /**
- * Convert an ARFF file to a VW file
+ * Utility class for converting an instance in ARFF format
+ * to an instance in the VW format.
  * @author cjb60
  */
 public class ArffToVW {
 	
+	/**
+	 * Convert an ARFF instance to a VW instance.
+	 * @param x a dense or sparse instance
+	 * @return a string returning the instance in VW format
+	 * @throws Exception if the dataset has a non-numeric or
+	 * non-nominal attribute (e.g. date, string)
+	 */
 	public static String process(Instance x) throws Exception {
-		// class label | feature_name:val nominal_feature ...
 		StringBuilder sb = new StringBuilder();
 		if( x.classAttribute().isNominal() ) {
 			if( x.numClasses() > 2 ) {
@@ -31,20 +39,38 @@ public class ArffToVW {
 			}
 		} else {
 			sb.append( x.classValue() + " | ");
-		}
-		for(int i = 0; i < x.numAttributes(); i++) {
-			if( i == x.classIndex() ) continue;
-			if( x.attribute(i).isNumeric() ) {
-				sb.append( x.attribute(i).name() + ":" + x.value(i) + " ");
-			} else if (x.attribute(i).isNominal() ) {
-				int idx = x.attribute(i).index();
-				String name = "is_" + x.attribute(i).name() + "_" + idx + " ";
-				sb.append(name);
-			} else {
-				throw new Exception("Attribute type not supported: " +
-						Attribute.typeToString( x.attribute(i) ));
+		}		
+		if( x instanceof DenseInstance) {
+			for(int i = 0; i < x.numAttributes(); i++) {
+				if( i == x.classIndex() ) continue;
+				if( x.attribute(i).isNumeric() ) {
+					sb.append( x.attribute(i).name() + ":" + x.value(i) + " ");
+				} else if (x.attribute(i).isNominal() ) {
+					if( (int)x.value(i) != 0 ) {
+						String name = "is_" + x.attribute(i).name() + "_" + ((int)x.value(i)) + " ";
+						sb.append(name);
+					}
+				} else {
+					throw new Exception("Attribute type not supported: " +
+							Attribute.typeToString( x.attribute(i) ));
+				}
+			}
+		} else {
+			for(int i = 0; i < x.numValues(); i++) {
+				int idx = x.index(i);
+				if( idx == x.classIndex() ) continue;
+				if( x.attribute(idx).isNumeric() ) {
+					sb.append( x.attribute(idx).name() + ":" + x.valueSparse(i) + " " );
+				} else if( x.attribute(idx).isNominal() ) {
+					String name = "is_" + x.attribute(idx).name() + "_" + ((int)x.valueSparse(i)) + " ";
+					sb.append(name);
+				} else {
+					throw new Exception("Attribute type not supported: " +
+							Attribute.typeToString( x.attribute(i) ));
+				}
 			}
 		}
+		
 		return sb.toString();
 	}
 	
