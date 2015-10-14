cef369d72b364596ed809b6a844bd77dfa6918ef
Christopher Beckham <chrispy645@gmail.com>
2015-10-14 15:35:19 +1300

    Add extra step to build and add img

--- build.sh
+++ build.sh
@@ -2,5 +2,7 @@
 
 ant clean
 #ant -noclasspath -lib $MOA_EXP_HOME/moa.jar:$WEKA_HOME/weka.jar exejar -Dpackage=moa-vw
-
+cd moa-modifications
+./build.sh
+cd ..
 ant -noclasspath -lib moa-modifications/src/main/java/:$MOA_HOME/moa.jar:$WEKA_HOME/weka.jar exejar -Dpackage=moa-vw
