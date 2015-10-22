4511686bd9ecb04598d15528d28f89ad8d9fc8ea
Christopher Beckham <chrispy645@gmail.com>
2015-10-23 09:11:12 +1300

    Fix indenting

--- moa-modifications/src/main/java/moa/streams/MetaArffFileStream.java
+++ moa-modifications/src/main/java/moa/streams/MetaArffFileStream.java
@@ -23,62 +23,62 @@ public class MetaArffFileStream extends ArffFileStream {
 	private static final long serialVersionUID = 2190789007689325368L;
 
 	private boolean m_firstTime = true;
-	
+
 	public MetaArffFileStream(String arffFileName, int classIndex) {
 		super(arffFileName, classIndex);
 	}
-	
+
 	public MetaArffFileStream() {
 		super();
 	}
-	
+
 	@Override
-    public boolean hasMoreInstances() {
+	public boolean hasMoreInstances() {
 		try {
-	        if (this.instances.readInstance(this.fileReader)) {
-	            this.lastInstanceRead = this.instances.instance(0);
-	            this.instances.delete(); // keep instances clean
-	            this.numInstancesRead++;
-	            return true;
-	        }
-	        if (this.fileReader != null) {
-	            this.fileReader.close();
-	            this.fileReader = null;
-	        }
+			if (this.instances.readInstance(this.fileReader)) {
+				this.lastInstanceRead = this.instances.instance(0);
+				this.instances.delete(); // keep instances clean
+				this.numInstancesRead++;
+				return true;
+			}
+			if (this.fileReader != null) {
+				this.fileReader.close();
+				this.fileReader = null;
+			}
 		} catch(Exception ex) {
 			ex.printStackTrace();
 			return false;
 		}
-        return false;
-    }
-	
+		return false;
+	}
+
 	@Override
-    public Instance nextInstance() {
-        return this.lastInstanceRead;
-    }
-	
-    @Override
-    public void restart() {
-        try {        		
-            if (this.fileReader != null) {
-                this.fileReader.close();
-            }
-            InputStream fileStream = new FileInputStream(this.arffFileOption.getFile());
-            this.fileProgressMonitor = new InputStreamProgressMonitor(
-                    fileStream);
-            this.fileReader = new BufferedReader(new InputStreamReader(
-                    this.fileProgressMonitor));
-            this.instances = new Instances(this.fileReader, 1);
-            if (this.classIndexOption.getValue() < 0) {
-                this.instances.setClassIndex(this.instances.numAttributes() - 1);
-            } else if (this.classIndexOption.getValue() > 0) {
-                this.instances.setClassIndex(this.classIndexOption.getValue() - 1);
-            }
-            this.numInstancesRead = 0;
-            this.lastInstanceRead = null;
-            //this.hitEndOfFile = !readNextInstanceFromFile();     	
-        } catch (IOException ioe) {
-            throw new RuntimeException("ArffFileStream restart failed.", ioe);
-        }
-    }
+	public Instance nextInstance() {
+		return this.lastInstanceRead;
+	}
+
+	@Override
+	public void restart() {
+		try {        		
+			if (this.fileReader != null) {
+				this.fileReader.close();
+			}
+			InputStream fileStream = new FileInputStream(this.arffFileOption.getFile());
+			this.fileProgressMonitor = new InputStreamProgressMonitor(
+					fileStream);
+			this.fileReader = new BufferedReader(new InputStreamReader(
+					this.fileProgressMonitor));
+			this.instances = new Instances(this.fileReader, 1);
+			if (this.classIndexOption.getValue() < 0) {
+				this.instances.setClassIndex(this.instances.numAttributes() - 1);
+			} else if (this.classIndexOption.getValue() > 0) {
+				this.instances.setClassIndex(this.classIndexOption.getValue() - 1);
+			}
+			this.numInstancesRead = 0;
+			this.lastInstanceRead = null;
+			//this.hitEndOfFile = !readNextInstanceFromFile();     	
+		} catch (IOException ioe) {
+			throw new RuntimeException("ArffFileStream restart failed.", ioe);
+		}
+	}
 }
